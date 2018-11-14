4cc228ca6555c7b8a98916a5325778fec643431a
Atmakafle1 <atma.kafle@gmail.com>
2018-11-14 09:05:22 -0600

    Add files via upload

---
+++ GetDayByDayNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMonthBy_MonthNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Week = new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+            for (int i = 1; i<= 7; i++)
+            {
+                
+                Console.WriteLine(Week[i-1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
