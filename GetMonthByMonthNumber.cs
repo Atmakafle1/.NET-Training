a7ef2de518f7c785d5d90882612269805d5b58fc
Atmakafle1 <atma.kafle@gmail.com>
2018-11-14 08:58:52 -0600

    Add files via upload

---
+++ GetMonthByMonthNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMonthBy_MonthNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Months = new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            for (int i = 1; i<= 12; i++)
+            {
+                
+                Console.WriteLine(Months[i-1]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
