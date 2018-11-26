0c15591c0672486b5255da2ced315bfde057ff15
Atmakafle1 <atma.kafle@gmail.com>
2018-11-26 09:43:18 -0600

    Add files via upload

---
+++ HelloName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Full Name");
+            String name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Hello" + " " + name);
+            Console.ReadKey();
+        }
+    }
+}
