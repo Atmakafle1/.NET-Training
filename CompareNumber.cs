1e6b3a976e2086ed93b15a88ae531e2f7860891e
Atmakafle1 <atma.kafle@gmail.com>
2018-11-28 12:26:00 -0600

    Add files via upload

---
+++ CompareNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    class CompareNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} is reater than {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("{0} is reater than {1}", b, a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
