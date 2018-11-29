c072b8ae31d275043bafc30e1fb0c4c028b61355
Atmakafle1 <atma.kafle@gmail.com>
2018-11-29 10:16:02 -0600

    ok

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            Console.WriteLine("Enter length of a rectangle:");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter width of a rectangle:");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            double Area = l * w;
+
+            Console.WriteLine("Rectangle area is: {0}", Area);
+
+            Console.WriteLine("Enter radius of a Sphere:");
+            double r = Convert.ToDouble(Console.ReadLine()); 
+            double volume = (4*pi*r*r*r)/ 3;
+            Console.WriteLine("Sphere volume is: {0}", volume);
+            Console.ReadLine();
+
+        }
+    }
+}
