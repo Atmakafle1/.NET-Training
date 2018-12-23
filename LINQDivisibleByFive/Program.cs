f9f18694c775442bb90c1f640184b578b58ada6c
Atma <atma.kafle@gmail.com>
2018-12-23 08:33:00 -0600

    ok

---
+++ LINQDivisibleByFive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDivisibleByFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 3, 6, 15, 21, 35, 75, 10 };
+            var numDivisibleByFive = from values in Array1
+                                     where (int)values % 5 == 0
+                                     select  values ;
+            foreach (var item in numDivisibleByFive)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQMaxNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQMaxNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 3, 4, 8, 12, 9, 74, 129, 2 };
+
+            var maxNumbers = from values in Array1
+                             orderby values descending
+                             select values;
+
+            foreach (int item in maxNumbers)
+            {
+                Console.WriteLine(item);
+                Console.ReadKey();
+            }
+        }
+    }
+}
